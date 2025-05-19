5642ac2b19fdee399c130ac3edaf62c13b274adb
Brayan Vilchez Daga <35672799+BrayanDaga@users.noreply.github.com>
2025-05-19 15:00:44 +0200

    Agregando mensajes

--- Controller.php
+++ Controller.php
@@ -6,121 +6,120 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader (created by composer, not included with PHPMailer)
+// Load Composer's autoloader (created by composer, not included with PHPMailer)
 require 'vendor/autoload.php';
 
 class Controller
 {
+    private String $name;
+    private String $email;
+    private String $body;
+    
+    private $errors = [];
+    private $msg= [];
+    
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
 
-    private string $name;
-    private string $email;
-    private string $message;
-    public $errors;
+    public function getMsg(): array
+    {
+        return $this->msg;
+    }
 
-    function  msgObligatorio($campo): String{
+    function msgObligatorio($campo): String
+    {
         return "El campo $campo es obligatorio";
     }
-    
-    function  msgMaxCaracteres($campo): String{
+
+    function msgMaxCaracteres($campo): String
+    {
         return "El campo $campo debe tener mas de 2 caracteres";
     }
-    
-    
-    function  enviarEmail():void{
-        
-        //Create an instance; passing `true` enables exceptions
-        
+
+    function enviarEmail(): void
+    {
+
+        // Create an instance; passing `true` enables exceptions
         try {
-            //Server settings
+            // Server settings
             $mail = new PHPMailer();
-            
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
             $mail->isSMTP();
             $mail->Host = 'sandbox.smtp.mailtrap.io';
             $mail->SMTPAuth = true;
             $mail->Port = 2525;
             $mail->Username = '5683a3d101750e';
             $mail->Password = 'e5e04fc973d14f';
-            
-            
-            
-            //Recipients
-            $mail->setFrom('from@example.com', 'Mailer');
-           // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-            $mail->addAddress('intertelecenter@yahoo.com');               //Name is optional
+
+            // Recipients
+            $mail->setFrom($this->email, $this->name);            // $mail->addAddress('joe@example.net', 'Joe User'); //Add a recipient
+            $mail->addAddress('intertelecenter@yahoo.com'); // Name is optional
             $mail->addReplyTo('intertelecenter@yahoo.com', 'Information');
-            //$mail->addCC('cc@example.com');
-            ///$mail->addBCC('bcc@example.com');
+            // $mail->addCC('cc@example.com');
+            // /$mail->addBCC('bcc@example.com');
+
+            // Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
+
+            // Content
+            $mail->isHTML(true); // Set email format to HTML
+            $mail->Subject = 'Nuevo mensaje desde el formulario de contacto';
             
-            //Attachments
-            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            $mail->Body = "<h2>Mensaje de contacto</h2>
+               <p><strong>Nombre:</strong> $this->name</p>
+               <p><strong>Email:</strong> $this->email</p>
+               <p><strong>Mensaje:</strong><br>$this->body</p>";
             
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = "<p>Nombre: $this->name</p><p>Email: $this->email</p><p>Mensaje: $this->message</p>";
-            $mail->AltBody = "Nombre: $this->name , Email: $this->email , Mensaje: $this->message";
-            
+            $mail->AltBody = "Nombre: $this->name\nEmail: $this->email\nMensaje:\n$this->body";
             
             $mail->send();
-            echo 'Message has been sent';
+            $this->msg = ['success'=>'Message has been sent'];
         } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            $this->msg = ['error'=> "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
         }
     }
 
-    function validar(): array
+    function validar(): void
     {
-        $errors = [];
-        
+
         if (empty($this->email)) {
-            $errors[] = $this->msgObligatorio("email");
-        }else{
+            $this->errors[] = $this->msgObligatorio("email");
+        } else {
             if (! filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
-                $errors[] = "El email no es válido";
+                $this->errors[] = "El email no es válido";
             }
         }
-        
-        
-        if(empty($this->name)){
-            $errors[] = $this->msgObligatorio("nombre");            
-        }else{
-            if(strlen($this->name) < 2 ){
-                $errors[] = $this->msgMaxCaracteres("nombre");
+
+        if (empty($this->name)) {
+            $this->errors[] = $this->msgObligatorio("nombre");
+        } else {
+            if (strlen($this->name) < 2) {
+                $this->errors[] = $this->msgMaxCaracteres("nombre");
             }
         }
-        
-        if(empty($this->message)){
-            $errors[] =  $this->msgObligatorio("mensaje");
-        }else{
-            if(strlen($this->message) < 2 ){
-                $errors[] = $this->msgMaxCaracteres("mensaje");
+
+        if (empty($this->body)) {
+            $this->errors[] = $this->msgObligatorio("mensaje");
+        } else {
+            if (strlen($this->body) < 2) {
+                $this->errors[] = $this->msgMaxCaracteres("mensaje");
             }
         }
-       
-        if(!empty($errors)){
-            return $errors;
-        }else{
-            return [];
-        }
-       
     }
 
-    
-    
-    
     function __construct()
     {
         $this->email = htmlspecialchars($_POST['email']);
-        $this->message = htmlspecialchars($_POST['message']);
+        $this->body = htmlspecialchars($_POST['body']);
         $this->name = htmlspecialchars($_POST['name']);
-        $this->errors = $this->validar();
-        
-        if(count($this->errors)==0 ){
+        $this->validar();
+        if (count($this->errors) == 0) {
             $this->enviarEmail();
         }
-        
     }
 }
 
--- index.php
+++ index.php
@@ -1,50 +1,72 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    
-    include_once 'Controller.php';
-    $controller = null;
-    if ($_POST != null) {
-    $controller =  new Controller();
-   
-    }
-    
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once 'Controller.php';
+$controller = null;
+if ($_POST != null) {
+    $controller = new Controller();
+    $errors = $controller->getErrors();
+    $msg = $controller->getMsg();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Contacto</title>
-    <link rel="stylesheet"  type="text/css" href="style.css">
-   	
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Formulario de Contacto</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
-    <h1>Formulario de Contacto</h1>
+	<h1>Formulario de Contacto</h1>
     
-   <?php if ($controller && !empty($controller->errors)): ?>
-    <ul class="errors">
-        <?php foreach ($controller->errors as $mensaje): ?>
-            <li><?php echo htmlspecialchars($mensaje); ?></li>
-        <?php endforeach; ?>
-    </ul>
+<?php
+if ($controller) :
+    // Mostrar errores si existen
+    if (!empty($errors)) : ?>
+        <ul class="errors">
+            <?php foreach ($errors as $mensaje): ?>
+                <li><?= htmlspecialchars($mensaje); ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+    <?php if (!empty($msg)) : ?>
+        <?php if (!empty($msg['error'])) : ?>
+            <div class="errors">
+                <p><?= htmlspecialchars($msg['error']); ?></p>
+            </div>
+        <?php endif; ?>
+
+        <?php if (!empty($msg['success'])) : ?>
+            <div class="success">
+                <p><?= htmlspecialchars($msg['success']); ?></p>
+            </div>
+        <?php endif; ?>
+    <?php endif; ?>
 <?php endif; ?>
+
+   
+ 
    
     
     
     <form action="index.php" method="post">
-        <label for="name">Nombre:</label>
-        <input type="text" id="name" name="name" 
-        value="<?=  empty($_POST['name']) ? "": htmlspecialchars($_POST['name'])  ?>" > 
+		<label for="name">Nombre:</label> <input type="text" id="name"
+			name="name"
+			value="<?=  empty($_POST['name']) ? "": htmlspecialchars($_POST['name'])  ?>">
 
-        <label for="email">Correo Electrónico:</label>
-        <input type="text" id="email" name="email"  
-        value="<?=  empty($_POST['email']) ? "": htmlspecialchars($_POST['email'])  ?>" >
+		<label for="email">Correo Electrónico:</label> <input type="text"
+			id="email" name="email"
+			value="<?=  empty($_POST['email']) ? "": htmlspecialchars($_POST['email'])  ?>">
 
-        <label for="message">Mensaje:</label>
-        <textarea id="message" name="message" rows="5" ><?=  empty($_POST['message']) ? "": htmlspecialchars($_POST['message'])  ?></textarea>
+		<label for="body">Mensaje:</label>
+		<textarea id="body" name="body" rows="5"><?=  empty($_POST['body']) ? "": htmlspecialchars($_POST['body'])  ?></textarea>
 
-        <button type="submit">Enviar</button>
-    </form>
+		<button type="submit">Enviar</button>
+	</form>
 </body>
 </html>
